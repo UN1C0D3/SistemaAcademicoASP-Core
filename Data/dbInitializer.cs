50682822725ac0ad1adf3b813b4b6e5a2673ad50
UN1C0D3 <hunter_tigre1998@hotmail.com>
2020-02-02 03:37:37 -0400

    Add files via upload

---
+++ Data/dbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using Sistema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Data
+{
+    public class dbInitializer
+    {
+        public static void Initialize(SistemaContext Context)
+        {
+            Context.Database.EnsureCreated();
+
+            //Busqueda de registros
+
+            if(Context.Categoria.Any())
+            {
+                return;
+            }
+
+            var categorias = new Categoria[]
+            {
+                new Categoria{Nombre="Programacion", Descripcion="Cursos de Programacion (insertado por defecto)", Estado= true},
+                new Categoria{Nombre="Diseño grafico", Descripcion="Cursos de Diseño Grafico (insertado por defecto)", Estado= true}
+            };
+
+            foreach (Categoria c in categorias)
+            {
+                Context.Categoria.Add(c);
+            }
+
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        [Required (ErrorMessage = "Campo Requerido")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage ="Ingrese un nombre entre 3-50 caracteres")]
+        public string Nombre { get; set; }
+        [StringLength(256, ErrorMessage ="Solamente 256 caracteres")]
+        [Display(Name ="Descripción")]
+        public string Descripcion { get; set; }
+        public Boolean Estado { get; set; }
+    }
+}
